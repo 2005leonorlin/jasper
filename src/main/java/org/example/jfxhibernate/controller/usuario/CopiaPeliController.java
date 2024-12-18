cfea77656f161918654850d86b4b020c5b829753
LeonorLinacero <2005leonorlin@gmail.com>
2024-12-18 19:58:46 +0100

    commi de leoflix

--- src/main/java/org/example/jfxhibernate/controller/usuario/CopiaPeliController.java
+++ src/main/java/org/example/jfxhibernate/controller/usuario/CopiaPeliController.java
@@ -183,7 +183,7 @@ public class CopiaPeliController implements Initializable {
     }
 
     /**
-     * Genera un informe con los datos de la copia  que  seleccionas..
+     * Genera un informe con los datos de la copia  que  seleccionas.
      * @param actionEvent
      */
     @javafx.fxml.FXML
